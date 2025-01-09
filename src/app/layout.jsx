9e7b3d79f6a5403abda4c6e53d04f5161860f3d6
Shomari <nyusternie@sdot.io>
2025-01-09 05:01:23 -0500

    Update media + metadata.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,36 +7,38 @@ import '@/styles/tailwind.css'
 import PlausibleProvider from 'next-plausible'
 
 export const metadata = {
-  title: {
-    template: '%s - Protocol API Reference',
-    default: 'Protocol API Reference',
-  },
+    title: {
+        template: '%s - Castr GÜRŲ Docs',
+        default: 'Castr GÜRŲ Docs',
+    },
 }
 
 export default async function RootLayout({ children }) {
-  let pages = await glob('**/*.mdx', { cwd: 'src/app' })
-  let allSectionsEntries = await Promise.all(
-    pages.map(async (filename) => [
-      '/' + filename.replace(/(^|\/)page\.mdx$/, ''),
-      (await import(`./${filename}`)).sections,
-    ]),
-  )
-  let allSections = Object.fromEntries(allSectionsEntries)
+    let pages = await glob('**/*.mdx', { cwd: 'src/app' })
 
-  return (
-    <html lang="en" className="h-full" suppressHydrationWarning>
-      <body className="flex min-h-full bg-white antialiased dark:bg-zinc-900">
-        <PlausibleProvider
-          domain="docs.castr.guru"
-          customDomain="https://plausible.castr.guru"
-        >
-          <Providers>
-            <div className="w-full">
-              <Layout allSections={allSections}>{children}</Layout>
-            </div>
-          </Providers>
-        </PlausibleProvider>
-      </body>
-    </html>
-  )
+    let allSectionsEntries = await Promise.all(
+        pages.map(async (filename) => [
+            '/' + filename.replace(/(^|\/)page\.mdx$/, ''),
+            (await import(`./${filename}`)).sections,
+        ]),
+    )
+
+    let allSections = Object.fromEntries(allSectionsEntries)
+
+    return (
+        <html lang="en" className="h-full" suppressHydrationWarning>
+            <body className="flex min-h-full bg-white antialiased dark:bg-zinc-900">
+                <PlausibleProvider
+                    domain="docs.castr.guru"
+                    customDomain="https://plausible.castr.guru"
+                >
+                    <Providers>
+                        <div className="w-full">
+                            <Layout allSections={allSections}>{children}</Layout>
+                        </div>
+                    </Providers>
+                </PlausibleProvider>
+            </body>
+        </html>
+    )
 }
--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -4,34 +4,35 @@ import { useEffect } from 'react'
 import { ThemeProvider, useTheme } from 'next-themes'
 
 function ThemeWatcher() {
-  let { resolvedTheme, setTheme } = useTheme()
+    let { resolvedTheme, setTheme } = useTheme()
 
-  useEffect(() => {
-    let media = window.matchMedia('(prefers-color-scheme: dark)')
+    useEffect(() => {
+        let media = window.matchMedia('(prefers-color-scheme: dark)')
 
-    function onMediaChange() {
-      let systemTheme = media.matches ? 'dark' : 'light'
-      if (resolvedTheme === systemTheme) {
-        setTheme('system')
-      }
-    }
+        function onMediaChange() {
+            let systemTheme = media.matches ? 'dark' : 'light'
 
-    onMediaChange()
-    media.addEventListener('change', onMediaChange)
+            if (resolvedTheme === systemTheme) {
+                setTheme('system')
+            }
+        }
 
-    return () => {
-      media.removeEventListener('change', onMediaChange)
-    }
-  }, [resolvedTheme, setTheme])
+        onMediaChange()
+        media.addEventListener('change', onMediaChange)
 
-  return null
+        return () => {
+            media.removeEventListener('change', onMediaChange)
+        }
+    }, [resolvedTheme, setTheme])
+
+    return null
 }
 
 export function Providers({ children }) {
-  return (
-    <ThemeProvider attribute="class" disableTransitionOnChange>
-      <ThemeWatcher />
-      {children}
-    </ThemeProvider>
-  )
+    return (
+        <ThemeProvider attribute="class" disableTransitionOnChange>
+            <ThemeWatcher />
+            {children}
+        </ThemeProvider>
+    )
 }
